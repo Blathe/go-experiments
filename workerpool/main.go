6cc8a0a964b4de7188ddd633790c335c3075c5ef
Scott Peters <scottpeters2281@gmail.com>
2023-10-24 19:58:39 -0700

    refactoring some code on workerpool experiment

--- workerpool/main.go
+++ workerpool/main.go
@@ -8,12 +8,10 @@ import (
 )
 
 type job struct {
-	Mux             sync.RWMutex
-	Index           int
-	StartMessage    string
-	Duration        int //in seconds
-	CompleteMessage string
-	Complete        bool
+	Mux      sync.RWMutex
+	Index    int
+	Duration int //in seconds
+	Complete bool
 }
 
 type worker struct {
@@ -24,20 +22,24 @@ type worker struct {
 }
 
 type workerpool struct {
-	Jobs             []*job
-	Workers          []*worker
-	AvailableWorkers []*worker
+	Jobs    []*job
+	Workers []*worker
 }
 
-func (wp *workerpool) HaveWorkersFindJobs(jp *[]*job) {
-	for _, worker := range wp.AvailableWorkers {
+type jobpool struct {
+	Mux  sync.RWMutex
+	Jobs []*job
+}
+
+func (wp *workerpool) HaveWorkersFindJobs(jp *jobpool) {
+	for _, worker := range wp.Workers {
 		if worker.HasJob == false {
 			worker.CheckForMoreJobs(jp)
 		}
 	}
 }
 
-func (w *worker) StartJob(j *job, pool *[]*job) {
+func (w *worker) StartJob(j *job, pool *jobpool) {
 	j.Mux.Lock()
 	j.Complete = true
 	j.Mux.Unlock()
@@ -49,16 +51,18 @@ func (w *worker) StartJob(j *job, pool *[]*job) {
 
 	sleepTime := time.Duration(j.Duration) * time.Second
 	time.Sleep(sleepTime)
-	*pool = append((*pool)[:j.Index], (*pool)[j.Index:]...)
+	pool.Mux.Lock()
+	pool.Jobs = append(pool.Jobs[:j.Index], pool.Jobs[j.Index+1:]...)
+	pool.Mux.Unlock()
 	fmt.Printf("Worker %v finished job %v\n", w.Index, j.Index)
-	fmt.Printf("Removing job %v from pool...\n", j.Index)
+	fmt.Printf("Removing job %v from pool...%v jobs left...\n", j.Index, len(pool.Jobs))
 	w.HasJob = false
 
 	w.CheckForMoreJobs(pool)
 }
 
-func (w *worker) CheckForMoreJobs(pool *[]*job) {
-	for _, j := range *pool {
+func (w *worker) CheckForMoreJobs(pool *jobpool) {
+	for _, j := range pool.Jobs {
 		if j.Complete == false {
 			w.StartJob(j, pool)
 			return
@@ -71,25 +75,26 @@ func (w *worker) CheckForMoreJobs(pool *[]*job) {
 }
 
 func main() {
-	var jobPool []*job
+	var jobs []*job
 	min := 1
 	max := 2
 
 	workerPool := workerpool{
-		Workers:          nil,
-		AvailableWorkers: nil,
-		Jobs:             jobPool,
+		Workers: nil,
+		Jobs:    jobs,
+	}
+
+	jobPool := jobpool{
+		Jobs: jobs,
 	}
 
 	for i := 0; i < 5000; i++ {
 		newJob := job{
-			Index:           i,
-			StartMessage:    fmt.Sprintf("Job %v started...", i),
-			Duration:        rand.Intn(max-min) + min,
-			CompleteMessage: fmt.Sprintf("Job %v complete!", i),
-			Complete:        false,
+			Index:    i,
+			Duration: rand.Intn(max-min) + min,
+			Complete: false,
 		}
-		jobPool = append(jobPool, &newJob)
+		jobPool.Jobs = append(jobPool.Jobs, &newJob)
 	}
 
 	for i := 0; i < 7; i++ {
@@ -100,20 +105,19 @@ func main() {
 			WorkerPool: &workerPool,
 		}
 		workerPool.Workers = append(workerPool.Workers, &newWorker)
-		workerPool.AvailableWorkers = append(workerPool.AvailableWorkers, &newWorker)
 
 		fmt.Println("Spawning worker...")
-		go newWorker.StartJob(jobPool[i], &jobPool)
+		go newWorker.StartJob(jobPool.Jobs[i], &jobPool)
 	}
 
 	time.Sleep(3 * time.Second)
-	AddMoreJobs(&jobPool)
+	AddMoreJobs(jobPool.Jobs)
 	time.Sleep(8 * time.Second)
-	AddMoreJobs(&jobPool)
+	AddMoreJobs(jobPool.Jobs)
 	fmt.Scanln()
 }
 
-func AddMoreJobs(pool *[]*job) {
+func AddMoreJobs(pool []*job) {
 
 	fmt.Println("More jobs being added to the pool... -------------------")
 
@@ -122,12 +126,10 @@ func AddMoreJobs(pool *[]*job) {
 
 	for i := 0; i < 20; i++ {
 		newJob := job{
-			Index:           i,
-			StartMessage:    fmt.Sprintf("Job %v started...", i),
-			Duration:        rand.Intn(max-min) + min,
-			CompleteMessage: fmt.Sprintf("Job %v complete!", i),
-			Complete:        false,
+			Index:    i,
+			Duration: rand.Intn(max-min) + min,
+			Complete: false,
 		}
-		*pool = append(*pool, &newJob)
+		pool = append(pool, &newJob)
 	}
 }
